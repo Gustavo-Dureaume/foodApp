ab78dc1136bde46e274c664ac6cf791a8a539ca7
Gustavo-Dureaume <gustavodureaume@gmail.com>
2024-12-05 17:17:06 -0300

    incorporation of page

---
+++ foodApp/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingViewController.swift
+//  foodApp
+//
+//  Created by gustavo on 05/12/2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
